1072c47b89e27fb56f614c856202d7fe0648c100
lovetwice1012 <lovetwice1012@gmail.com>
2022-05-18 16:38:38 +0900

    秘密の部屋への入室を拒否した場合にリロードしないと再入室できない問題を修正

--- secret.js
+++ secret.js
@@ -11,6 +11,9 @@ document.addEventListener('keypress', (event) => {
         var result = window.confirm('あなたは秘密の部屋への入り口を見つけたようです...!\n秘密の部屋に移動しますか？');
         if(result){
             window.location.href = "secret.html";
+        }else{
+            count = 0;
+            alert("秘密の部屋への入り口を見失ってしまいました...\nもう一度同じ操作をすることで秘密の部屋への入り口を見つけられるかもしれません...!");
         }
     }
 });
