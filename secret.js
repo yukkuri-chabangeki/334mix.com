cced5a128b3a99cca8d4ab1873cdc4fd40b42214
lovetwice1012 <lovetwice1012@gmail.com>
2022-05-18 16:36:06 +0900

    デバッグ関数除去忘れ修正

--- secret.js
+++ secret.js
@@ -2,14 +2,11 @@ var count = 0;
 document.addEventListener('keypress', (event) => {
     var keyName = event.key;
     var secretWord = Array("y","u","k","k","u","r","i");
-    console.log(`keypress:${keyName}`);
-    console.log(secretWord[count]);
     if(secretWord[count] == keyName){
         count++;
     }else{
         count = 0;
     }
-    console.log(count);
     if(count == 7){
         var result = window.confirm('あなたは秘密の部屋への入り口を見つけたようです...!\n秘密の部屋に移動しますか？');
         if(result){
